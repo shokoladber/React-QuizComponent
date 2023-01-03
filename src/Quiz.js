3e7eb29ece0d1c98f29fb97e2fcf25232fe30e80
Michael Allen <93445521+shokoladber@users.noreply.github.com>
2023-01-03 15:13:36 -0600

    Module 1, not working yet

---
+++ src/Quiz.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+let quizData = require('./quiz_data,json');
+
+class Quiz extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {quiz_position: 1};
+    }
+
+    render() {
+        return
+        <div>
+            <className="QuizQuestion"
+            value="quizData[0].instructions_text"
+            />;
+        </div>;
+    };
+
+};
+
+export default Quiz;
